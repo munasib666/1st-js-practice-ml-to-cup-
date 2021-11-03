a37392b66cad817a000ac1651ede5556bfe4d802
Munasib <85922794+munasib666@users.noreply.github.com>
2021-11-03 12:59:16 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+
+function kiloweightConvert(value) {
+    document.getElementById("ml").style.color = "red"
+    document.getElementById("cup").style.color = "green";
+    document.getElementById("cup").innerHTML =
+    "cup : " + value/237;
+    
+}
